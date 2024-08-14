61d34483a814acdae3112302e0fec33b89ded197
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-14 23:18:00 +0300

    Upload Stack.cpp and Stack.h files

---
+++ 3- Stack/4- Infix-Posfix-Prefix/Stack.cpp
@@ -0,0 +1,140 @@
+
+#include "Stack.h"
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+template<class dataType>
+Stack<dataType>::Stack() {}
+
+/////////////////////////////////////////
+
+template<class dataType>
+Stack<dataType>::~Stack() {
+    list.clear() ;
+}
+
+/////////////////////////////////////////
+
+template<class dataType>
+bool Stack<dataType> :: isEmpty() {
+    return list.isEmpty() ;
+}
+
+/////////////////////////////////////////
+
+template<class dataType>
+void Stack<dataType> :: push(dataType element) {
+    list.insertAtHead(element) ;
+}
+
+/////////////////////////////////////////
+
+template<class dataType>
+void Stack<dataType>::pop() {
+    assert(!isEmpty()) ;
+    list.removeHead();
+}
+
+/////////////////////////////////////////
+
+template<class dataType>
+dataType Stack<dataType>::peek() {
+    assert(!isEmpty()) ;
+    return list.front();
+}
+
+/////////////////////////////////////////
+
+template<class dataType>
+void Stack<dataType>::print() {
+    list.print();
+}
+
+/////////////////////////////////////////
+
+template<class dataType>
+int Stack<dataType> :: precedence(char c) {
+    if (c == '+' || c == '-')
+        return 1;
+    if (c == '*' || c == '/')
+        return 2;
+    if (c == '^')
+        return 3;
+    return 0;
+}
+
+template<class dataType>
+string Stack<dataType> :: infixToPostfix(string input) {
+    string postfix = "" ;
+    Stack<char> operators ;
+
+    for (auto c : input) {
+        if (isdigit(c) || isalpha(c))
+            postfix += c ;
+        else if (c == '(')
+            operators.push(c) ;
+        else if (c == ')') {
+            while (!operators.isEmpty() && operators.peek() != '(' ) {
+                postfix += operators.peek() ;
+                operators.pop() ;
+            }
+            operators.pop() ; // remove -> (
+        }
+        else {
+            while (!operators.isEmpty() && precedence(c) <= precedence(operators.peek())) {
+                postfix += operators.peek() ;
+                operators.pop() ;
+            }
+            operators.push(c);
+        }
+    }
+
+    while (!operators.isEmpty()) {
+        postfix += operators.peek() ;
+        operators.pop() ;
+    }
+
+    return postfix ;
+}
+
+/////////////////////////////////////////
+
+template<class dataType>
+double Stack<dataType> :: result(double x , double y , char c ) {
+    if (c == '+')
+        return x + y ;
+    if (c == '*')
+        return x * y ;
+    if (c == '-')
+        return x - y ;
+    if (c == '/')
+        return x / y ;
+
+    return pow(x,y) ; // for ^
+}
+
+template<class dataType>
+double Stack<dataType> :: postfixEvaluation(string postfix) {
+    Stack<int> evaluation ;
+    for (auto c : postfix) {
+        if (isdigit(c))
+            evaluation.push(c-'0') ;
+        else {
+            double x = evaluation.peek() ;
+            evaluation.pop() ;
+
+            double y = evaluation.peek() ;
+            evaluation.pop() ;
+
+            double val = result(x , y , c ) ;
+            evaluation.push(val) ;
+        }
+    }
+
+    return evaluation.peek();
+}
+
+
---
+++ 3- Stack/4- Infix-Posfix-Prefix/Stack.h
@@ -0,0 +1,28 @@
+
+#ifndef STACK_H
+#define STACK_H
+#include <iostream>
+#include <vector>
+#include "SinglyLinkedList.cpp"
+using namespace std;
+
+template <class dataType>
+class Stack {
+private:
+    SinglyLinkedList<dataType> list ;
+public:
+    Stack();
+    ~Stack();
+    void push(dataType element) ;
+    void pop();
+    dataType peek();
+    void print() ;
+    bool isEmpty();
+    ///////////////////////////////////
+    int precedence(char c ) ;
+    string infixToPostfix(string input) ;
+    double postfixEvaluation(string posfix) ;
+    double result(double x , double y , char c ) ;
+};
+
+#endif //STACK_H
