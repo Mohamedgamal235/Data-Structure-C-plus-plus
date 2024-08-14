eb0ef2b55294892761a7a7cee50a71eeb26a7243
Mohamed Gamal Ali <mohamedgamal200420042004@gmail.com>
2024-08-14 12:37:01 +0300

    add files of Stack and Queue Codes

---
+++ 3- Stack/Stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mohamed Gamal on 14/08/2024.
+//
+
+#include "Stack.h"
---
+++ 3- Stack/Stack.h
@@ -0,0 +1,16 @@
+//
+// Created by Mohamed Gamal on 14/08/2024.
+//
+
+#ifndef STACK_H
+#define STACK_H
+
+
+
+class Stack {
+
+};
+
+
+
+#endif //STACK_H
