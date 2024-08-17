9d1184bf8049f592ede8079bb678cea3cf0ee558
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-17 20:19:27 +0300

    Updated main.cpp file

--- 5- Binary Trees/main.cpp
+++ 5- Binary Trees/main.cpp
@@ -11,10 +11,22 @@ int main() {
     tree.insert({2,4,8} , {'L' , 'L' , 'R'});
     tree.insert({3,6,10} , {'R' , 'R' , 'L'});
 
-
     tree.printInOrder() ; // 7 4 8 2 5 9 1 3 10 6
     tree.printInPostOrder(); // 7 8 4 9 5 2 10 6 3 1
     tree.printInPreOrder() ; // 1 2 4 7 8 5 9 3 6 10
+
+    cout << "==================\n" ;
+
+    BinaryTree<int> tree2(2) ;
+    tree2.insert({3} , {'L'}) ;
+    tree2.insert({13 , 7 } , {'R' , 'L'}) ;
+    tree2.insert({13, 8} , {'R' , 'R'}) ;
+    cout << tree2.maxElement() << '\n' ;
+
+    cout << "==================\n" ;
+
+    cout << tree2.heightOfTree() << '\n' ;
+    cout << tree2.numberOfNodes() << '\n' ;
 
     return  0 ;
 }
