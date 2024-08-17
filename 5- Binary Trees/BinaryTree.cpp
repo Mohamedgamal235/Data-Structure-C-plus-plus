696fe41a76b0697cf0bb689cb20afc634fe52a0a
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-17 18:06:17 +0300

    Updated Binary Tree files

--- 5- Binary Trees/BinaryTree.cpp
+++ 5- Binary Trees/BinaryTree.cpp
@@ -1,5 +1,6 @@
 #include "BinaryTree.h"
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 // ================= Implementation Helper Functions ======================= //
@@ -14,26 +15,109 @@ void BinaryTree<dataType>::printInOrderHelper(Node<dataType>* curr) {
     printInOrderHelper(curr->right) ;
 }
 
-/////////////////////////////////////////////////
+// ==================================================
+
+template<class dataType>
+void BinaryTree<dataType>::printInPostOrderHelper(Node<dataType>* curr) {
+    if (!curr)
+        return;
+    printInPostOrderHelper(curr->left) ;
+    printInPostOrderHelper(curr->right) ;
+    cout << curr->data << ' ' ;
+}
+
+// ==================================================
+
+template<class dataType>
+void BinaryTree<dataType>::printInPreOrderHelper(Node<dataType>* curr) {
+    if (!curr)
+        return;
+    cout << curr->data << ' ' ;
+    printInPreOrderHelper(curr->left) ;
+    printInPreOrderHelper(curr->right) ;
+}
+
+// ==================================================
 
 // ================= Implementation User Functions ========================= //
 // ========================================================================= //
 
 template<class dataType>
-BinaryTree<dataType>::BinaryTree() {
-
+BinaryTree<dataType>::BinaryTree(const dataType &val) {
+    root = new Node<dataType>(val) ;
 }
 
-
+//////////////////////////
 
 template<class dataType>
 BinaryTree<dataType>::~BinaryTree() {
 
 }
 
+//////////////////////////
 
 template<class dataType>
 void BinaryTree<dataType>::printInOrder() {
-
+    printInOrderHelper(root) ;
+    cout << '\n' ;
 }
+
+//////////////////////////
+
+template<class dataType>
+void BinaryTree<dataType>::printInPostOrder() {
+    printInPostOrderHelper(root) ;
+    cout << '\n' ;
+}
+
+//////////////////////////
+
+template<class dataType>
+void BinaryTree<dataType>::printInPreOrder() {
+    printInPreOrderHelper(root) ;
+    cout << '\n' ;
+}
+
+//////////////////////////
+
+template<class dataType>
+void BinaryTree<dataType>::insert(const vector<dataType> &values, const vector<char> &direction) {
+    assert(values.size() == direction.size()) ;
+
+    Node<dataType>* curr = this->root ;
+
+    for (int i = 0 ; i < direction.size() ; i++) {
+        if (direction[i] == 'L') {
+            if (curr->left == nullptr)
+                curr->left = new Node(values[i] );
+            else
+                assert(curr->left->data == values[i]);
+            curr = curr->left ;
+        }
+        else if ( direction[i] == 'R' ){
+            if (curr->right == nullptr)
+                curr->right = new Node(values[i]) ;
+            else
+                assert(curr->right->data == values[i]) ;
+            curr = curr->right ;
+        }
+        else {
+            cout << "Invalid diraction : " << direction[i] << '\n' ;
+            return ;
+        }
+    }
+}
+
+//////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
--- 5- Binary Trees/BinaryTree.h
+++ 5- Binary Trees/BinaryTree.h
@@ -1,15 +1,14 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
+#include <vector>
+using namespace std;
 
 template<class T>
 struct Node {
     T data ;
     Node<T>* left ;
     Node<T>* right ;
-    Node(T val) {
-        data = val ;
-        left = right = nullptr ;
-    }
+    Node(T val) : data(val) , left(nullptr) , right(nullptr) {}
 };
 
 template<class dataType>
@@ -17,10 +16,15 @@ class BinaryTree {
 private:
     Node<dataType>* root ;
     void printInOrderHelper(Node<dataType>* curr) ;
+    void printInPostOrderHelper(Node<dataType>* curr) ;
+    void printInPreOrderHelper(Node<dataType>* curr) ;
 public:
-    BinaryTree();
+    BinaryTree(const dataType &val);
     ~BinaryTree();
     void printInOrder() ;
+    void printInPostOrder() ;
+    void printInPreOrder() ;
+    void insert(const vector<dataType> &values , const vector<char> &direction ) ;
 };
 
 #endif //BINARYTREE_H
