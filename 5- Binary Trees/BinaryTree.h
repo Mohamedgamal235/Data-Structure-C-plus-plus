d3b6be45b72af440051f46cd9820f6b33f7327d3
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-17 22:31:41 +0300

    Updated ->add some functions on BinaryTree.h file

--- 5- Binary Trees/BinaryTree.h
+++ 5- Binary Trees/BinaryTree.h
@@ -18,19 +18,34 @@ private:
     void printInOrderHelper(Node<dataType>* curr) ;
     void printInPostOrderHelper(Node<dataType>* curr) ;
     void printInPreOrderHelper(Node<dataType>* curr) ;
+    
+    // ============== Extra Function for Practice if you want ============== //
     dataType maxElementHelper(Node<dataType>* curr) ;
     int heightOfTreeHelper(Node<dataType>* curr) ;
     int numberOfNodesHelper(Node<dataType>* curr) ;
-public:
+    int numberOfLeavesHelper(Node<dataType>* curr) ;
+    bool isExistHepler(Node<dataType>* curr , dataType value) ;
+    void clearHelper(Node<dataType>* curr) ;
+
+public: // ============== Public ============== //
+
+    // ============== Basic Function ============== //
     BinaryTree(const dataType &val);
     ~BinaryTree();
     void printInOrder() ;
     void printInPostOrder() ;
     void printInPreOrder() ;
     void insert(const vector<dataType> &values , const vector<char> &direction ) ;
+    bool isEmpty() ;
+
+    // ============== Extra Function for Practice if you want ============== //
     dataType maxElement() ;
     int heightOfTree();
     int numberOfNodes() ;
+    int numberOfLeaves() ;
+    bool isExist(dataType value) ;
+    void clear();
+
 };
 
 #endif //BINARYTREE_H
