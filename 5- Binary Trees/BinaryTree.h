203ed871dd96c721b2b4a5bd201f93dabeb46a21
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-17 11:37:26 +0300

    Updated Binary Tree class

--- 5- Binary Trees/BinaryTree.h
+++ 5- Binary Trees/BinaryTree.h
@@ -1,8 +1,26 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
-class BinaryTree {
+template<class T>
+struct Node {
+    T data ;
+    Node<T>* left ;
+    Node<T>* right ;
+    Node(T val) {
+        data = val ;
+        left = right = nullptr ;
+    }
+};
 
+template<class dataType>
+class BinaryTree {
+private:
+    Node<dataType>* root ;
+    void printInOrderHelper(Node<dataType>* curr) ;
+public:
+    BinaryTree();
+    ~BinaryTree();
+    void printInOrder() ;
 };
 
 #endif //BINARYTREE_H
