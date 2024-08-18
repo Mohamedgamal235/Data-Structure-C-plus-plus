0d93e197d2c2f354e4d8244cdd0fe145748b3a3d
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-18 18:14:56 +0300

    Upload Binary Tree After Updates

---
+++ 5- Binary Trees/2- Extra Fcuntions (Mendatory)/BinaryTree.h
@@ -0,0 +1,53 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+#include <vector>
+using namespace std;
+
+template<class T>
+struct Node {
+    T data ;
+    Node<T>* left ;
+    Node<T>* right ;
+    Node(T val) : data(val) , left(nullptr) , right(nullptr) {}
+};
+
+template<class dataType>
+class BinaryTree {
+private:
+    Node<dataType>* root ;
+    void printInOrderHelper(Node<dataType>* curr) ;
+    void printInPostOrderHelper(Node<dataType>* curr) ;
+    void printInPreOrderHelper(Node<dataType>* curr) ;
+
+    // ============== Extra Function for Practice if you want ============== //
+    void printInLevelOrderHelper(Node<dataType>* curr) ;
+    dataType maxElementHelper(Node<dataType>* curr) ;
+    int heightOfTreeHelper(Node<dataType>* curr) ;
+    int numberOfNodesHelper(Node<dataType>* curr) ;
+    int numberOfLeavesHelper(Node<dataType>* curr) ;
+    bool isExistHepler(Node<dataType>* curr , dataType value) ;
+    void clearHelper(Node<dataType>* curr) ;
+
+public: // ============== Public ============== //
+
+    // ============== Basic Function ============== //
+    BinaryTree(const dataType &val);
+    ~BinaryTree();
+    void printInOrder() ;
+    void printInPostOrder() ;
+    void printInPreOrder() ;
+    void insert(const vector<dataType> &values , const vector<char> &direction ) ;
+    bool isEmpty() ;
+
+    // ============== Extra Function for Practice if you want ============== //
+    void printInLevelOrder() ;
+    dataType maxElement() ;
+    int heightOfTree();
+    int numberOfNodes() ;
+    int numberOfLeaves() ;
+    bool isExist(dataType value) ;
+    void clear();
+
+};
+
+#endif //BINARYTREE_H
