451599966514989014aea3f85ff80563a842d003
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-21 22:51:28 +0300

    Update Class Binary heap

--- 7- Binary Heap/BinaryHeap.h
+++ 7- Binary Heap/BinaryHeap.h
@@ -1,16 +1,46 @@
-//
-// Created by Mohamed Gamal on 21/08/2024.
-//
-
 #ifndef BINARYHEAP_H
 #define BINARYHEAP_H
 
 
-
+template<class dataType>
 class BinaryHeap {
+protected: // to inherit this class for Min and Max Heap classes
+    dataType *arr ;
+    int top ;
+    int capacity ;
+
+    int parent(int pos ) ;
+    int leftChild(int pos ) ;
+    int rightChild(int pos ) ;
+
+    virtual void heapifyUp(int childPos);
+    virtual void heapifyDown(int childPos ) ;
+    void resizeHeap() ;
+
+public:
+    BinaryHeap() ;
+    ~BinaryHeap();
+    void clear() ;
+    int size() ; 
+    void insert(dataType value) ;
 
 };
 
 
+template<class dataType>
+class MinHeap : public BinaryHeap<dataType>{
+protected:
+    void heapifyUp(int childPos);
+    void heapifyDown(int childPos);
+
+};
+
+
+template<class dataType>
+class MaxHeap : public BinaryHeap<dataType>{
+protected:
+    void heapifyUp(int childPos);
+    void heapifyDown(int childPos);
+};
 
 #endif //BINARYHEAP_H
