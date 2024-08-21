6a9fd237e2e2c6d26113e54370b73cc44c42ae95
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-21 18:17:39 +0300

    Make Folder for binary heap

---
+++ 7- Binary Heap/BinaryHeap.h
@@ -0,0 +1,16 @@
+//
+// Created by Mohamed Gamal on 21/08/2024.
+//
+
+#ifndef BINARYHEAP_H
+#define BINARYHEAP_H
+
+
+
+class BinaryHeap {
+
+};
+
+
+
+#endif //BINARYHEAP_H
---
+++ 7- Binary Heap/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "BinaryHeap.cpp"
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
