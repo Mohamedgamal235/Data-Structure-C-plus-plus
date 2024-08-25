0e6a994798d6781f77cbbe0cd81426eed4012994
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-25 21:01:42 +0300

    Add : implementation struct functions

--- 8- AVL Tree/AVLTree.cpp
+++ 8- AVL Tree/AVLTree.cpp
@@ -1,1 +1,88 @@
 #include "AVLTree.h"
+using namespace std;
+
+// ======================================================================================================== //
+// ================================== Implementation Struct Functions ===================================== //
+// ======================================================================================================== //
+
+
+template<class dataType>
+Node<dataType>::Node(dataType data) {
+    this->data = data ;
+    left = nullptr ;
+    right = nullptr ;
+    heghit = 0 ;
+}
+
+//------------
+
+template<class dataType>
+int Node<dataType>::childHeghit(Node<dataType> *node) {
+    if (!node)
+        return -1 ; // for null and updateHeghit function to make it zero for leaf node
+    return node->heghit ;
+}
+
+//------------
+
+template<class dataType>
+int Node<dataType>::updateHeghit() {
+    heghit = 1 + max(childHeghit(left) , childHeghit(right)) ;
+    return heghit ;
+}
+
+//------------
+
+template<class dataType>
+int Node<dataType>::balanceFactor() {
+    return childHeghit(left) - childHeghit(right) ;
+}
+
+//------------
+
+
+// ======================================================================================================== //
+// ================================== Implementation Class Functions ====================================== //
+// ======================================================================================================== //
+
+
+// ============================================================ //
+// ============== Implementation Helper Functions ============= //
+// ============================================================ //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
