0a2c63a2e3bd03dd1e23dacfbb302722f65ee26a
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-26 23:18:46 +0300

    implementation of insert functions

--- 9- Letter Tree (Trie)/Trie.cpp
+++ 9- Letter Tree (Trie)/Trie.cpp
@@ -1,1 +1,58 @@
 #include "Trie.h"
+
+#include <string.h>
+
+const int Trie::MAX_CHAR = 26 ;
+
+//------------
+
+Trie::Trie() {
+    isLeaf = false ;
+    // make set an array to zeros .
+    memset(child , 0 , sizeof(child)) ;
+}
+
+//------------
+
+Trie::~Trie() {
+    delete[] child ;
+}
+
+//------------
+
+void Trie::insert(string str, int idx) {
+    if (idx == str.size()) {
+        isLeaf = true ;
+        return ;
+    }
+
+    int curr = str[idx] - 'a' ;
+    if (child[curr] == 0)
+        child[curr] = new Trie() ;
+    child[curr]->insert(str , idx + 1 ) ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
