d998d01cfc1421b04e23b503007dd34510abecfd
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-26 23:50:50 +0300

    Test functions of Trie class

--- 9- Letter Tree (Trie)/main.cpp
+++ 9- Letter Tree (Trie)/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+
+#include "Trie.h"
 using namespace std;
 
 int main() {
 
+    Trie str ;
+
+    str.insert("abcd") ;
+    str.insert("xyz") ;
+    str.insert("abf") ;
+
+    cout << "=== 0 -> False ==== 1 -> True ===\n" ;
+
+
+    cout << "\n=== Test wordExist function ==== \n" ;
+    cout << "abc : " << str.wordExist("abc") << '\n' ;
+    cout << "abcd : " << str.wordExist("abcd") << '\n' ;
+    cout << "abf : " << str.wordExist("abf") << '\n' ;
+
+    cout << "=============================\n" ;
+
+    cout << "=== Test prefixExist function ==== \n" ;
+    cout << "ab : " << str.prefixExist("ab") << '\n' ;
+    cout << "bf : " << str.prefixExist("bf") << '\n' ;
+    cout << "af : " << str.prefixExist("af") << '\n' ;
+    cout << "xz : " << str.prefixExist("xz") << '\n' ;
 
 
     return 0;
