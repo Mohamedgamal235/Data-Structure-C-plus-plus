7b8c9060eb9007433ee8083099669df1c5c368dd
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-26 23:44:53 +0300

    Update The class

--- 9- Letter Tree (Trie)/Trie.h
+++ 9- Letter Tree (Trie)/Trie.h
@@ -2,22 +2,21 @@
 #define TRIE_H
 
 #include <iostream>
-using namespace std ;
+#include <cstring>
+using namespace std;
 
 class Trie {
 private:
-    static const int MAX_CHAR ;
+    static const int MAX_CHAR = 26;
     Trie* child[MAX_CHAR];
-    bool isLeaf ;
+    bool isLeaf;
 
 public:
     Trie();
-    ~Trie() ;
-    void insert(string str , int idx = 0) ;
-    bool wordExist(string str , int idx = 0 ) ;
-    bool prefixExist(string str , int idx = 0) ;
+    ~Trie();
+    void insert(string str, int idx = 0);
+    bool wordExist(string str, int idx = 0);
+    bool prefixExist(string str, int idx = 0);
 };
-
-
 
 #endif //TRIE_H
