955bbbd25a3786409f9bf03afb7e8c24332f7b17
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-26 17:04:35 +0300

    Make some updates

--- 7- AVL Tree/AVLTree.h
+++ 7- AVL Tree/AVLTree.h
@@ -3,44 +3,46 @@
 
 template<class dataType>
 struct Node {
-    int heghit ;
-    dataType data ;
-    Node<dataType>* left ;
-    Node<dataType>* right ;
+    int height;  
+    dataType data;
+    Node<dataType>* left;
+    Node<dataType>* right;
     Node(dataType data);
-    int childHeghit(Node<dataType> * node) ;
-    int updateHeghit();
+    int childHeight(Node<dataType> * node); 
+    int updateHeight();  
     int balanceFactor();
 };
 
 template<class dataType>
 class AVLTree {
 private:
-    Node<dataType>* root ;
+    Node<dataType>* root;
     void inOrder(Node<dataType>* curr);
     void inPre(Node<dataType>* curr);
     void inPost(Node<dataType>* curr);
-    Node<dataType>* leftRotation(Node<dataType>* P) ;
-    Node<dataType>* rightRotation(Node<dataType>* Q) ;
-    Node<dataType>* makeBalance(Node<dataType>* curr) ;
-    Node<dataType>* insertHelper(dataType value , Node<dataType>* curr) ;
-    Node<dataType>* specialDelete(Node<dataType> *curr  , Node<dataType>* child) ;
-    Node<dataType>* removeHelper(Node<dataType>* curr , dataType value) ;
-    dataType getMaxHelper(Node<dataType>* curr) ;
-    dataType getMinHelper(Node<dataType>* curr) ;
-    void clearHelper(Node<dataType>* curr) ;
+    //////////////////////// For AVL Tree /////////////////////////////////////////
+    Node<dataType>* leftRotation(Node<dataType>* P);
+    Node<dataType>* rightRotation(Node<dataType>* Q);
+    Node<dataType>* makeBalance(Node<dataType>* curr);
+    //////////////////////////////////////////////////////////////////////////////
+    Node<dataType>* insertHelper(dataType value, Node<dataType>* curr);
+    Node<dataType>* specialDelete(Node<dataType> *curr, Node<dataType>* child);
+    Node<dataType>* removeHelper(Node<dataType>* curr, dataType value);
+    dataType getMaxHelper(Node<dataType>* curr);
+    dataType getMinHelper(Node<dataType>* curr);
+    void clearHelper(Node<dataType>* curr);
 
 public:
     AVLTree();
     ~AVLTree();
-    void insert(dataType value) ;
-    void remove(dataType value) ;
-    dataType getMax() ;
-    dataType getMin() ;
-    void printInOrder() ;
-    void printInPreOrder() ;
-    void printInPostOrder() ;
-    void clear() ;
+    void insert(dataType value);
+    void remove(dataType value);
+    dataType getMax();
+    dataType getMin();
+    void printInOrder();
+    void printInPreOrder();
+    void printInPostOrder();
+    void clear();
 };
 
 
