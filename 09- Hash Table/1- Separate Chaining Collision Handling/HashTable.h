b848eaa314fc658fe8524f6925b03de08513b489
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-28 00:06:44 +0300

    Full class for basic functions

--- 09- Hash Table/1- Separate Chaining Collision Handling/HashTable.h
+++ 09- Hash Table/1- Separate Chaining Collision Handling/HashTable.h
@@ -1,13 +1,31 @@
-
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
+#include <iostream>
+#include <vector>
+using namespace std ;
 
+struct StudentEntry {
+private:
+    static const int INTERNAL_LIMIT = 6000 ;
+    string name ;
+    string studentID ;
 
-class StudentEntry {
-
+    StudentEntry(string name , string studentID) ;
+    int hashFunction() ;
+    void print() ;
 };
 
 class HashTable {
+private:
+    int tableSize ;
+    vector<vector<string>> table ;
+
+public:
+    HashTable(int size);
+    void insert(StudentEntry &student) ;
+    bool search(StudentEntry &student) ;
+    void remove(StudentEntry &student) ;
+    void print() ; 
 
 };
 
