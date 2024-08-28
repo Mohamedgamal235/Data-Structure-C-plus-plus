7fb825b01921f0c663886e1570efbfee6a742a6b
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-28 16:38:15 +0300

    Implement hash string for hash function

--- 09- Hash Table/1- Separate Chaining Collision Handling/HashTable.cpp
+++ 09- Hash Table/1- Separate Chaining Collision Handling/HashTable.cpp
@@ -1,2 +1,21 @@
-
 #include "HashTable.h"
+
+
+long long hashString(string &str , int n) {
+    long long bigN = n ;
+    long long sum = 0 ;
+    for (int i = 0 ; i < str.size() ; i++) {
+        sum = ( sum * 26 + str[i] - 'a' ) % bigN ;
+    }
+    return sum % bigN ;
+}
+
+StudentEntry::StudentEntry(string name, string studentID) {
+    this->name = name ;
+    this->studentID = studentID ;
+}
+
+int StudentEntry::hashFunction() {
+    return hashString(name , INTERNAL_LIMIT) ;
+}
+
