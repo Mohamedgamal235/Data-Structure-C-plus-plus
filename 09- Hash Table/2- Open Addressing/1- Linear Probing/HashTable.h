014a4f50fca1f468de38bc409e775fd536fbcbaf
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-28 22:37:52 +0300

    Initail class and struct

--- 09- Hash Table/2- Open Addressing/1- Linear Probing/HashTable.h
+++ 09- Hash Table/2- Open Addressing/1- Linear Probing/HashTable.h
@@ -1,13 +1,37 @@
-
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
 
-class StudentEntry {
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+struct StudentData {
+public:
+    static const int INTERNAL_LIMIT = 6000;
+    string name ;            // key
+    string studentID ;       // value
+
+    StudentData(string name, string studentID);
+    int hashFunction() const;
+    void print() const;
 };
 
 class HashTable {
+private:
+    int tableSize;
+    vector<StudentData> table ;
+    vector<bool> occupied ;
+    int next(int idx) ;
+public:
+    HashTable(int size);
+    ~HashTable();
+    void insert(const StudentData& student);
+    bool search(StudentData& student) const;
+    void remove(const StudentData &student);
+    void print() const;
 
 };
 
