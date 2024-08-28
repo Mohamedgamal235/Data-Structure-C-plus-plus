727aa324bd93767efb3090ee275ab24e45ab7029
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-28 23:39:57 +0300

    implement functions of struct , insert , next , constructor and destructor

--- 09- Hash Table/2- Open Addressing/2- Quadratic Probing/HashTable.cpp
+++ 09- Hash Table/2- Open Addressing/2- Quadratic Probing/HashTable.cpp
@@ -1,2 +1,56 @@
-
 #include "HashTable.h"
+
+long long hashString(const string& str, int n) {
+    long long bigN = n;
+    long long sum = 0;
+    for (int i = 0; i < (int)str.size(); i++) {
+        sum = (sum * 26 + str[i] - 'a') % bigN;
+    }
+    return sum % bigN;
+}
+
+// ==================================================== //
+// =========== Implementation of Struct =============== //
+// ==================================================== //
+
+StudentData::StudentData(string name, string studentID) {
+    this->name = name;
+    this->studentID = studentID;
+}
+
+int StudentData::hashFunction() const {
+    return hashString(name, INTERNAL_LIMIT);
+}
+
+void StudentData::print() const {
+    cout << "Name: " << name << " , ID: " << studentID ;
+}
+
+// ==================================================== //
+// =========== Implementation of Class ================ //
+// ==================================================== //
+
+
+HashTable::HashTable(int size) : tableSize(size), table(size), occupied(size, false) {}
+
+//---------------
+
+HashTable::~HashTable() {
+    table.clear();
+    occupied.clear();
+    tableSize = 0;
+}
+
+//---------------
+
+int HashTable::quadraticProbing(int idx , int i) const {
+    return (idx + i * i) % tableSize ;
+}
+
+
+
+
+
+
+
+
