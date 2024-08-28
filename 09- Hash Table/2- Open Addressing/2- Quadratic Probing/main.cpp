6c4945eddbd7af82b48fdf277e28c0da057292cc
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-28 23:51:55 +0300

    Test Hash table class

---
+++ 09- Hash Table/2- Open Addressing/2- Quadratic Probing/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "HashTable.h"
+using namespace std;
+
+int main() {
+
+
+    HashTable table(7) ;
+
+    table.insert(StudentData("mona" , "2649")) ;        // 3
+    table.insert(StudentData("amr" , "200314")) ;       // 0
+    table.insert(StudentData("ali" , "200135")) ;       // 0
+    table.insert(StudentData("mohand" , "200365")) ;    // 0
+    table.insert(StudentData("mohsen" , "20220365")) ;  // 0
+    table.insert(StudentData("mohamed" , "20220284")) ; // 3
+    table.insert(StudentData("mena" , "26548")) ;       // 6
+
+    table.print() ;
+
+    cout << "===================================================\n" ;
+    table.remove(StudentData("mohamed" , "20220284" )) ;
+    table.remove(StudentData("ali" , "200135")) ;
+    cout << "After remove mohamed and ali : \n" ;
+
+
+    table.print() ;
+
+    cout << "===================================================\n" ;
+    cout << "Search for mohamed : " << table.search(StudentData("mohamed" , "20220284")) << '\n' ;
+    cout << "Search for mena : " << table.search(StudentData("mena" , "26548")) << '\n' ;
+
+
+    return 0;
+}
