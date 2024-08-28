d6171b97207ce931acb9f57d37571e3bc239eaaf
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-28 23:39:12 +0300

    inital Hash table class

--- 09- Hash Table/2- Open Addressing/2- Quadratic Probing/HashTable.h
+++ 09- Hash Table/2- Open Addressing/2- Quadratic Probing/HashTable.h
@@ -1,16 +1,36 @@
-
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
 
-class StudentEntry {
+struct StudentData {
+public:
+    static const int INTERNAL_LIMIT = 6000;
+    string name;            // key
+    string studentID;       // value
 
+    StudentData(string name = "", string studentID = "");
+    int hashFunction() const;
+    void print() const;
 };
 
 class HashTable {
+private:
+    int tableSize;
+    vector<StudentData> table;
+    vector<bool> occupied;
+    int quadraticProbing(int idx , int i) const;
 
+public:
+    HashTable(int size);
+    ~HashTable();
+    void insert(const StudentData& student);
+    bool search(const StudentData& student) const;
+    void remove(const StudentData& student);
+    void print() const;
 };
-
-
 
 #endif //HASHTABLE_H
