1d986baa3f7fdf9aae82dcb521e86cc76a6f1670
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-08-28 17:35:52 +0300

    Implementation Print function and update some bugs

--- 09- Hash Table/1- Separate Chaining Collision Handling/HashTable.cpp
+++ 09- Hash Table/1- Separate Chaining Collision Handling/HashTable.cpp
@@ -1,59 +1,59 @@
 #include "HashTable.h"
 
-
-long long hashString(string &str , int n) {
-    long long bigN = n ;
-    long long sum = 0 ;
-    for (int i = 0 ; i < str.size() ; i++) {
-        sum = ( sum * 26 + str[i] - 'a' ) % bigN ;
+long long hashString(const string& str, int n) {
+    long long bigN = n;
+    long long sum = 0;
+    for (int i = 0; i < (int)str.size(); i++) {
+        sum = (sum * 26 + str[i] - 'a') % bigN;
     }
-    return sum % bigN ;
+    return sum % bigN;
 }
 
 // ==================================================== //
-// =========== Implementation of  Struct ============== //
+// =========== Implementation of Struct =============== //
 // ==================================================== //
 
 StudentData::StudentData(string name, string studentID) {
-    this->name = name ;
-    this->studentID = studentID ;
+    this->name = name;
+    this->studentID = studentID;
 }
 
-int StudentData::hashFunction() {
-    return hashString(name , INTERNAL_LIMIT) ;
+int StudentData::hashFunction() const {
+    return hashString(name, INTERNAL_LIMIT);
 }
 
+void StudentData::print() const {
+    cout << "Name: " << name << "\nID: " << studentID << "\n==============\n";
+}
 
 // ==================================================== //
-// =========== Implementation of  Class =============== //
+// =========== Implementation of Class ================ //
 // ==================================================== //
 
-void HashTable::insert(StudentData &student) {
-    int key = student.hashFunction() % tableSize ;
+HashTable::HashTable(int size) : tableSize(size), table(size) {}
 
-    for (auto &data : table[key]) {
+void HashTable::insert(const StudentData& student) {
+    int key = student.hashFunction() % tableSize;
+
+    for (auto& data : table[key]) {
         if (data.name == student.name) {
-            student = data ;
+            data = student; 
             return;
         }
     }
 
-    table[key].push_back(student) ; 
+    table[key].push_back(student);
 }
 
 
+void HashTable::print() const {
+    for (int hash = 0; hash < tableSize; hash++) {
+        if (table[hash].empty())
+            continue;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        cout << "Hash " << hash << ": ";
+        for (const auto& student : table[hash]) {
+            student.print();
+        }
+    }
+}
