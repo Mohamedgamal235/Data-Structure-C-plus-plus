4e43dcc39a416cef6e841ea28903c3454adfc8d7
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-10-18 18:18:41 +0300

    Add Basic function of

--- Advanced Data Structure/Red-Black Tree/RedBlackTree.h
+++ Advanced Data Structure/Red-Black Tree/RedBlackTree.h
@@ -2,6 +2,9 @@
 #ifndef REDBLACKTREE_H
 #define REDBLACKTREE_H
 
+#include <iostream>
+using namespace std ;
+
 template<class dataType>
 struct Node {
     dataType data ;
@@ -14,13 +17,30 @@ struct Node {
 };
 
 
-template<class dataTyep>
+template<class dataType>
 class RedBlackTree {
 private:
-    Node<dataTyep>* root ;
-public:
-};
+    Node<dataType>* root;
+    void inOrder(Node<dataType>* curr);
+    void inPre(Node<dataType>* curr);
+    void inPost(Node<dataType>* curr);
+    Node<dataType>* rightRotation(Node<dataType>* child);
+    Node<dataType>* leftRotation(Node<dataType>* parent) ;
+    Node<dataType>* insertHelper(Node<dataType>* curr , dataType value) ;
+    Node<dataType>* getMaxHelper(Node<dataType>* curr) ;
+    Node<dataType>* getMinHelper(Node<dataType>* curr) ;
+    void clearHelper(Node<dataType>* curr) ;
 
+public:
+    RedBlackTree();
+    ~RedBlackTree();
+    void printInOrder();
+    void printInPreOrder();
+    void printInPostOrder();
+    void insert(dataType value) ;
+    void clear() ;
+
+};
 
 
 #endif //REDBLACKTREE_H
