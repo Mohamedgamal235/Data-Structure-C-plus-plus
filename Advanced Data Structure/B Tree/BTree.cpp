054b57e8e673c5a422605b2d08d1502de8e8982e
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-12-07 21:56:21 +0200

    implement insert and insertHelper and splitChild function

--- Advanced Data Structure/B Tree/BTree.cpp
+++ Advanced Data Structure/B Tree/BTree.cpp
@@ -6,7 +6,64 @@ using namespace std ;
 // ========================================================================= //
 
 
+template<class dataType, int ORDER>
+void BTree<dataType, ORDER>::insertHelper(Node<dataType, ORDER> *curr, dataType val) {
+    int i = curr->numOfKeys - 1 ;
 
+    if (curr->isLeaf) {
+        while (i >= 0 && val < curr->keys[i]) { // make shifts
+            curr->keys[i+1] = curr->keys[i];
+            i-- ;
+        }
+        curr->keys[i+1] = val ;
+        curr->numOfKeys++ ;
+    }
+    else {
+        while (i >=0 && val < curr->keys[i])
+            i-- ;
+        i++ ; // to make it at index of i + 1 to insert value okay
+
+        if (curr->children[i].numOfKeys == ORDER - 1) {
+            splitChild(curr , i);
+
+
+            if (val > curr->keys[i])
+                i++ ;
+
+        }
+        insertHelper(curr->children[i] , val);
+    }
+}
+
+template<class dataType, int ORDER>
+void BTree<dataType, ORDER>::splitChild(Node<dataType, ORDER> *parent, int idx) {
+    Node<dataType , ORDER>* fullChild = parent->children[idx] ;
+    Node<dataType , ORDER>* newChild = new Node<dataType , ORDER>(fullChild->isLeaf); // to make it on same level
+
+    newChild->numOfKeys = ORDER / 2 - 1 ;
+
+    for (int j = 0 ; j < ORDER / 2 - 1 ; j++)
+        newChild->keys[j] = fullChild->keys[j+ORDER/2];
+
+    if (!fullChild->isLeaf) {
+        for (int j = 0 ; j < ORDER / 2 ; j++)
+            newChild->children[j] = fullChild->children[j + ORDER / 2] ;
+    }
+
+    fullChild->numOfKeys = ORDER / 2 - 1 ;
+
+    for (int j = parent->numOfKeys ; j >= idx + 1 ; j--)
+        parent->children[j+1] = parent->children[j] ;
+
+    parent->children[idx + 1] = newChild ;
+
+    for (int i = parent->numOfKeys - 1 ; i >= idx ; i--)
+        parent->keys[i+1] = parent->keys[i] ;
+
+
+    parent->keys[idx] = fullChild->keys[ORDER / 2 - 1] ;
+    parent->numOfKeys++ ; 
+}
 
 
 
@@ -25,7 +82,7 @@ using namespace std ;
 
 template<class dataType, int ORDER>
 BTree<dataType, ORDER>::BTree() {
-    root = new Node<dataType , ORDER>(true) ;
+    root = nullptr;
 }
 
 template<class dataType, int ORDER>
@@ -35,9 +92,15 @@ BTree<dataType, ORDER>::~BTree() {
 
 template<class dataType, int ORDER>
 void BTree<dataType, ORDER>::insert(dataType val) {
-    if (!root) {
-
+    if (root->numOfKeys == ORDER - 1) {
+        Node<dataType , ORDER>* newRoot = new Node<dataType, ORDER>(false) ;
+        newRoot->children[0] = root ;
+        splitChild(newRoot , 0) ;
+        root = newRoot ;
+        insertHelper(root , val);
     }
+    else
+        insertHelper(root , val);
 }
 
 template<class dataType, int ORDER>
