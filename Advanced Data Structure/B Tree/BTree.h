02821a8dc5066ce01ee5f12768bf2376ccab776a
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-12-07 22:46:29 +0200

    Update Class implementation and header

--- Advanced Data Structure/B Tree/BTree.h
+++ Advanced Data Structure/B Tree/BTree.h
@@ -1,171 +1,47 @@
-#include "BTree.h"
-using namespace std ;
+#ifndef BTREE_H
+#define BTREE_H
+#include <bits/range_access.h>
 
-// ========================================================================= //
-// ================= Implementation Helper Functions ======================= //
-// ========================================================================= //
-
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::insertHelper(Node<dataType, ORDER> *curr, dataType val) {
-    int i = curr->numOfKeys - 1 ;
-
-    if (curr->isLeaf) {
-        while (i >= 0 && val < curr->keys[i]) { // make shifts
-            curr->keys[i+1] = curr->keys[i];
-            i-- ;
-        }
-        curr->keys[i+1] = val ;
-        curr->numOfKeys++ ;
-    }
-    else {
-        while (i >=0 && val < curr->keys[i])
-            i-- ;
-        i++ ; // to make it at index of i + 1 to insert value okay
-
-        if (curr->children[i].numOfKeys == ORDER - 1) {
-            splitChild(curr , i);
-
-
-            if (val > curr->keys[i])
-                i++ ;
-
-        }
-        insertHelper(curr->children[i] , val);
-    }
-}
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::splitChild(Node<dataType, ORDER> *parent, int idx) {
-    Node<dataType , ORDER>* fullChild = parent->children[idx] ;
-    Node<dataType , ORDER>* newChild = new Node<dataType , ORDER>(fullChild->isLeaf); // to make it on same level
-
-    newChild->numOfKeys = ORDER / 2 - 1 ;
-
-    for (int j = 0 ; j < ORDER / 2 - 1 ; j++)
-        newChild->keys[j] = fullChild->keys[j+ORDER/2];
-
-    if (!fullChild->isLeaf) {
-        for (int j = 0 ; j < ORDER / 2 ; j++)
-            newChild->children[j] = fullChild->children[j + ORDER / 2] ;
+template<class dataType , int ORDER>
+struct Node {
+    dataType keys[ORDER - 1] ;
+    Node * children ;
+    int numOfKeys ;
+    bool isLeaf ;
+    Node(bool isLeaf) {
+        numOfKeys = 0 ;
+        this.isLeaf = isLeaf ;
+        this->children = new dataType [ORDER];
+        for (int i = 0 ; i < ORDER ; i++)
+            children[i] = nullptr ;
     }
 
-    fullChild->numOfKeys = ORDER / 2 - 1 ;
+};
 
-    for (int j = parent->numOfKeys ; j >= idx + 1 ; j--)
-        parent->children[j+1] = parent->children[j] ;
+template<class dataType , int ORDER>
+class BTree {
+private:
 
-    parent->children[idx + 1] = newChild ;
-
-    for (int i = parent->numOfKeys - 1 ; i >= idx ; i--)
-        parent->keys[i+1] = parent->keys[i] ;
-
-
-    parent->keys[idx] = fullChild->keys[ORDER / 2 - 1] ;
-    parent->numOfKeys++ ;
-}
-
-
-template<class dataType, int ORDER>
-Node<dataType, ORDER> *BTree<dataType, ORDER>::searchHelper(Node<dataType, ORDER> *curr, dataType val) {
-    int idx = 0 ;
-    while (idx < curr->numOfKeys && val > curr->keys[idx])
-        idx++ ;
-
-    if (idx < curr->numOfKeys && val == curr->keys[idx])
-        return curr ;
-
-    if (curr->isLeaf)
-        return nullptr ;
-
-    return searchHelper(curr->children[idx] , val) ;
-}
-
-template<class dataType, int ORDER>
-dataType BTree<dataType, ORDER>::getSuccessor(Node<dataType, ORDER> *node, int i) {
-    Node<dataType , ORDER>* curr = node->children[i + 1] ;
-    while (!curr->isLeaf)
-        curr = curr->children[0];
-    return curr->keys[0];
-}
-
-template<class dataType, int ORDER>
-dataType BTree<dataType, ORDER>::getPredecessor(Node<dataType, ORDER> *node, int i) {
-    Node<dataType , ORDER>* curr = node->children[i] ;
-    while (!curr->isLeaf)
-        curr = curr->children[curr->numOfKeys];
-    return curr->keys[curr->numOfKeys-1];
-}
+    Node<dataType , ORDER>* root ;
+    void insertHelper(Node<dataType , ORDER> * curr , dataType val);
+    void removeHelper(Node<dataType , ORDER> * curr );
+    Node<dataType , ORDER>* searchHelper(Node<dataType , ORDER> * curr , dataType val);
+    dataType getSuccessor(Node<dataType , ORDER>* curr , int i);
+    dataType getPredecessor(Node<dataType , ORDER>* curr , int i);
+    void splitChild(Node<dataType , ORDER> * curr, int i) ;
+    void clearHelper(Node<dataType , ORDER> * curr);
 
 
+public:
 
-// ========================================================================= //
-// ================= Implementation User Functions ========================= //
-// ========================================================================= //
+    BTree();
+    ~BTree();
+    void insert(dataType val) ;
+    dataType search(dataType val) ;
+    void remove();
+    void print();
+    void clear();
 
+};
 
-template<class dataType, int ORDER>
-BTree<dataType, ORDER>::BTree() {
-    root = nullptr;
-}
-
-template<class dataType, int ORDER>
-BTree<dataType, ORDER>::~BTree() {
-    clear();
-}
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::insert(dataType val) {
-    if (root->numOfKeys == ORDER - 1) {
-        Node<dataType , ORDER>* newRoot = new Node<dataType, ORDER>(false) ;
-        newRoot->children[0] = root ;
-        splitChild(newRoot , 0) ;
-        root = newRoot ;
-        insertHelper(root , val);
-    }
-    else
-        insertHelper(root , val);
-}
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::remove() {
-
-}
-
-template<class dataType, int ORDER>
-dataType BTree<dataType, ORDER>::search(dataType val) {
-    if(!root)
-        return dataType();
-
-    Node<dataType , ORDER>* res = searchHelper(root , val);
-    if(res == nullptr)
-        return dataType();
-
-    return val ;
-}
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::print() {
-
-}
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::clear() {
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#endif //BTREE_H
