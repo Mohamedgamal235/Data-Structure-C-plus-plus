64cb02b977ff6a9e649ec1f0be9ab2e3f1edb6cc
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-12-07 21:26:21 +0200

    Initialize class and struct for B Tree

--- Advanced Data Structure/B Tree/BTree.h
+++ Advanced Data Structure/B Tree/BTree.h
@@ -1,16 +1,48 @@
-//
-// Created by Mohamed Gamal on 18/10/2024.
-//
-
 #ifndef BTREE_H
 #define BTREE_H
+#include <bits/range_access.h>
 
-
-
-class BTree {
+template<class dataType , int ORDER>
+struct Node {
+    dataType keys[ORDER - 1] ;
+    Node * children ;
+    int numOfKeys ;
+    bool isLeaf ;
+    Node(bool isLeaf) {
+        numOfKeys = 0 ;
+        this.isLeaf = isLeaf ;
+        this->children = new dataType [ORDER];
+        for (int i = 0 ; i < ORDER ; i++)
+            children[i] = nullptr ;
+    }
 
 };
 
+template<class dataType , int ORDER>
+class BTree {
+private:
 
+    BTree<dataType , ORDER>* root ;
+    void insertHelper(Node<dataType , ORDER> * curr , dataType val);
+    void removeHelper(Node<dataType , ORDER> * curr );
+    BTree<dataType , ORDER> * searchHelper(Node<dataType , ORDER> * curr , dataType val);
+    dataType successorHelper(Node<dataType , ORDER>* curr , dataType val);
+    void splitChild(Node<dataType , ORDER> * curr, int i) ;
+    void clearHelper(Node<dataType , ORDER> * curr);
+
+
+public:
+
+    BTree();
+    ~BTree();
+    void insert(dataType val) ;
+    dataType search(dataType val) ;
+    void remove();
+    void print();
+    void clear();
+    bool isEmpty();
+    bool isFull();
+
+};
 
 #endif //BTREE_H
