cee2763a3c23a7d801e8b3239684a8c91a2c9f7b
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-12-18 00:49:04 +0200

    Updated BTree.h header file

--- Advanced Data Structure/B Tree/BTree.h
+++ Advanced Data Structure/B Tree/BTree.h
@@ -1,47 +1,41 @@
+//
+// Created by Mohamed Gamal on 18/12/2024.
+//
+
 #ifndef BTREE_H
 #define BTREE_H
-#include <bits/range_access.h>
+
+
+
 
 template<class dataType , int ORDER>
-struct Node {
-    dataType keys[ORDER - 1] ;
-    Node * children ;
-    int numOfKeys ;
-    bool isLeaf ;
-    Node(bool isLeaf) {
-        numOfKeys = 0 ;
-        this.isLeaf = isLeaf ;
-        this->children = new dataType [ORDER];
-        for (int i = 0 ; i < ORDER ; i++)
-            children[i] = nullptr ;
-    }
+struct Node{
+    int numOfKeys;
+    int pos = -1;
+    dataType* keys;
+    Node** children;
 
+    Node ();
+    bool insertHelper (dataType value);
+    Node* split (Node* node, dataType* mid);
+    ~Node ();
 };
 
 template<class dataType , int ORDER>
-class BTree {
+class BTree{
 private:
-
-    Node<dataType , ORDER>* root ;
-    void insertHelper(Node<dataType , ORDER> * curr , dataType val);
-    void removeHelper(Node<dataType , ORDER> * curr );
-    Node<dataType , ORDER>* searchHelper(Node<dataType , ORDER> * curr , dataType val);
-    dataType getSuccessor(Node<dataType , ORDER>* curr , int i);
-    dataType getPredecessor(Node<dataType , ORDER>* curr , int i);
-    void splitChild(Node<dataType , ORDER> * curr, int i) ;
-    void clearHelper(Node<dataType , ORDER> * curr);
-
+    Node<dataType , ORDER> *root;
+    void printHelper(Node<dataType, ORDER>* curr, int space);
 
 public:
-
-    BTree();
-    ~BTree();
-    void insert(dataType val) ;
-    dataType search(dataType val) ;
-    void remove();
-    void print();
-    void clear();
-
+    BTree ();
+    ~BTree ();
+    void insert (dataType value);
+    void print() ;
 };
+
+
+
+
 
 #endif //BTREE_H
