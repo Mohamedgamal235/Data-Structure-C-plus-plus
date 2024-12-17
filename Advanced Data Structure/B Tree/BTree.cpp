059a0a43f3ad0c0711add4ee191f95bf35c97725
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-12-18 00:48:20 +0200

    Update BTree.cpp file BCz some bugs

--- Advanced Data Structure/B Tree/BTree.cpp
+++ Advanced Data Structure/B Tree/BTree.cpp
@@ -1,171 +1,156 @@
-#include "BTree.h"
+#include "../BTree/BTree.h"
+#include <iostream>
 using namespace std ;
 
-// ========================================================================= //
-// ================= Implementation Helper Functions ======================= //
-// ========================================================================= //
+//Node implementation
+template<class dataType , int ORDER>
+Node<dataType , ORDER>::Node (){
+    numOfKeys = 0;
+    keys = new dataType[ORDER];
+    children = new Node*[ORDER+1];
+    for (int i = 0; i <= ORDER; ++i)
+        children[i] = nullptr;
+}
 
 
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::insertHelper(Node<dataType, ORDER> *curr, dataType val) {
-    int i = curr->numOfKeys - 1 ;
+template<class dataType , int ORDER>
+bool Node<dataType , ORDER>::insertHelper(dataType value){
+    if (children[0] == nullptr){
+        pos++ ;
+        numOfKeys++;
+        keys[pos] = value;
+        for(int i = pos ; i > 0 ; i--)
+            if (keys[i] < keys[i-1])
+                swap(keys[i-1],keys[i]);
+    }
+    else{ // add to proper child heeerrreeeee
+        int i = 0 ;
+        while (i < numOfKeys && value > keys[i])
+            i++ ;
 
-    if (curr->isLeaf) {
-        while (i >= 0 && val < curr->keys[i]) { // make shifts
-            curr->keys[i+1] = curr->keys[i];
-            i-- ;
+        bool needSplit = children[i]->insertHelper(value);
+        if(needSplit){
+            dataType mid;
+            int childIndex = i; // child
+            Node<dataType , ORDER> *newNode = split(children[i], &mid);
+            for(; i < numOfKeys && mid > keys[i];)
+                i++;
+
+            for (int j = numOfKeys; j > i ; j--)
+                keys[j] = keys[j - 1];
+            keys[i] = mid;
+            ++numOfKeys;
+            ++pos;
+
+            int k;
+            for (k = numOfKeys; k > childIndex + 1; k--)
+                children[k] = children[k - 1];
+            children[k] = newNode;
         }
-        curr->keys[i+1] = val ;
-        curr->numOfKeys++ ;
+
+    }
+    if(numOfKeys == ORDER)
+        return true ;
+
+    return false ;
+}
+
+
+template<class dataType , int ORDER>
+Node<dataType , ORDER>* Node<dataType , ORDER>::split(Node *node, dataType *mid) {
+    int NumberOfKeys = node->numOfKeys;
+    Node<dataType , ORDER> *newNode = new Node<dataType , ORDER>;
+    int midIndex = NumberOfKeys / 2;
+    *mid = node->keys[midIndex];
+    int i;
+
+    // after half ya ragel
+    for (i = midIndex + 1; i < NumberOfKeys; ++i){
+        newNode->pos = newNode->pos + 1 ; // be 0
+        newNode->keys[newNode->pos] = node->keys[i];
+        newNode->children[newNode->pos] = node->children[i];
+        newNode->numOfKeys = newNode->numOfKeys + 1 ;
+        node->pos = node->pos - 1 ;
+        node->numOfKeys = node->numOfKeys - 1 ;
+        node->children[i] = nullptr;
+    }
+    newNode->children[newNode->pos + 1] = node->children[i];
+    node->children[i] = nullptr; // after increment i
+
+    node->numOfKeys = node->numOfKeys - 1 ; //because we take mid-value...
+    node->pos = node->pos - 1 ;
+    return newNode;
+}
+
+template<class dataType , int ORDER>
+Node<dataType , ORDER>::~Node (){
+    for (int i = 0; i <= numOfKeys; ++i)
+        delete children[i];
+    delete[] children;
+    delete[]keys;
+}
+
+
+
+template<class dataType , int ORDER>
+BTree<dataType , ORDER>::BTree(){
+    root  = nullptr;
+}
+
+template<class dataType , int ORDER>
+void BTree<dataType , ORDER>::insert(dataType value){
+    if (root == nullptr){
+        root = new Node<dataType , ORDER>;
+        root->pos++ ;
+        root->keys[root->pos] = value;
+        root->numOfKeys = 1;
     }
     else {
-        while (i >=0 && val < curr->keys[i])
-            i-- ;
-        i++ ; // to make it at index of i + 1 to insert value okay
-
-        if (curr->children[i].numOfKeys == ORDER - 1) {
-            splitChild(curr , i);
-
-
-            if (val > curr->keys[i])
-                i++ ;
-
+        bool needSplit = root->insertHelper(value);
+        if(needSplit){
+            dataType mid;
+            Node<dataType , ORDER> *splitNode = root->split(root, &mid);
+            Node<dataType , ORDER> *newRoot = new Node<dataType , ORDER>;
+            newRoot->pos++ ; // to be 0
+            newRoot->keys[newRoot->pos] = mid;
+            newRoot->numOfKeys=1;
+            newRoot->children[0] = root;
+            newRoot->children[1] = splitNode;
+            root = newRoot;
         }
-        insertHelper(curr->children[i] , val);
     }
 }
 
+
 template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::splitChild(Node<dataType, ORDER> *parent, int idx) {
-    Node<dataType , ORDER>* fullChild = parent->children[idx] ;
-    Node<dataType , ORDER>* newChild = new Node<dataType , ORDER>(fullChild->isLeaf); // to make it on same level
+void BTree<dataType, ORDER>::printHelper(Node<dataType, ORDER>* curr, int space) {
+    if (curr == nullptr)
+        return;
 
-    newChild->numOfKeys = ORDER / 2 - 1 ;
+    for (int i = 0; i < space; ++i)
+        cout << "  ";
 
-    for (int j = 0 ; j < ORDER / 2 - 1 ; j++)
-        newChild->keys[j] = fullChild->keys[j+ORDER/2];
-
-    if (!fullChild->isLeaf) {
-        for (int j = 0 ; j < ORDER / 2 ; j++)
-            newChild->children[j] = fullChild->children[j + ORDER / 2] ;
+    for (int i = 0; i < curr->numOfKeys; ++i) {
+        cout << curr->keys[i];
+        if (i < curr->numOfKeys - 1)
+            cout << ",";
     }
+    cout << endl;
 
-    fullChild->numOfKeys = ORDER / 2 - 1 ;
-
-    for (int j = parent->numOfKeys ; j >= idx + 1 ; j--)
-        parent->children[j+1] = parent->children[j] ;
-
-    parent->children[idx + 1] = newChild ;
-
-    for (int i = parent->numOfKeys - 1 ; i >= idx ; i--)
-        parent->keys[i+1] = parent->keys[i] ;
-
-
-    parent->keys[idx] = fullChild->keys[ORDER / 2 - 1] ;
-    parent->numOfKeys++ ;
-}
-
-
-template<class dataType, int ORDER>
-Node<dataType, ORDER> *BTree<dataType, ORDER>::searchHelper(Node<dataType, ORDER> *curr, dataType val) {
-    int idx = 0 ;
-    while (idx < curr->numOfKeys && val > curr->keys[idx])
-        idx++ ;
-
-    if (idx < curr->numOfKeys && val == curr->keys[idx])
-        return curr ;
-
-    if (curr->isLeaf)
-        return nullptr ;
-
-    return searchHelper(curr->children[idx] , val) ;
-}
-
-template<class dataType, int ORDER>
-dataType BTree<dataType, ORDER>::getSuccessor(Node<dataType, ORDER> *node, int i) {
-    Node<dataType , ORDER>* curr = node->children[i + 1] ;
-    while (!curr->isLeaf)
-        curr = curr->children[0];
-    return curr->keys[0];
-}
-
-template<class dataType, int ORDER>
-dataType BTree<dataType, ORDER>::getPredecessor(Node<dataType, ORDER> *node, int i) {
-    Node<dataType , ORDER>* curr = node->children[i] ;
-    while (!curr->isLeaf)
-        curr = curr->children[curr->numOfKeys];
-    return curr->keys[curr->numOfKeys-1];
-}
-
-
-
-// ========================================================================= //
-// ================= Implementation User Functions ========================= //
-// ========================================================================= //
-
-
-template<class dataType, int ORDER>
-BTree<dataType, ORDER>::BTree() {
-    root = nullptr;
-}
-
-template<class dataType, int ORDER>
-BTree<dataType, ORDER>::~BTree() {
-    clear();
-}
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::insert(dataType val) {
-    if (root->numOfKeys == ORDER - 1) {
-        Node<dataType , ORDER>* newRoot = new Node<dataType, ORDER>(false) ;
-        newRoot->children[0] = root ;
-        splitChild(newRoot , 0) ;
-        root = newRoot ;
-        insertHelper(root , val);
+    for (int i = 0; i <= curr->numOfKeys; ++i) {
+        if (curr->children[i] != nullptr)
+            printHelper(curr->children[i], space + 1);
     }
-    else
-        insertHelper(root , val);
 }
 
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::remove() {
-
-}
-
-template<class dataType, int ORDER>
-dataType BTree<dataType, ORDER>::search(dataType val) {
-    if(!root)
-        return dataType();
-
-    Node<dataType , ORDER>* res = searchHelper(root , val);
-    if(res == nullptr)
-        return dataType();
-
-    return val ;
-}
 
 template<class dataType, int ORDER>
 void BTree<dataType, ORDER>::print() {
-
-}
-
-template<class dataType, int ORDER>
-void BTree<dataType, ORDER>::clear() {
-
+    printHelper(root, 0);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+template <class dataType,int ORDER>
+BTree<dataType , ORDER>::~BTree (){
+    delete root;
+}
